8d19c8bd31e5f6d8c27449e59ed4613d762817e8
sampath88 <sampathreddy8896@gmail.com>
2023-07-27 12:22:36 +0530

    updated folder structure

---
+++ Week-12/todo-app/src/components/todo.jsx
@@ -0,0 +1,5 @@
+const Todo = (props) => {
+  return <div>Todo</div>;
+};
+
+export default Todo;
---
+++ Week-12/todo-app/src/pages/todos-page.jsx
@@ -0,0 +1,11 @@
+import Todo from "@components/todo";
+
+const TodosPage = (props) => {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+};
+
+export default TodosPage;
