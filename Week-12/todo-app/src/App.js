b33d0e1638249cd77e26a37191ff4190e6f0fbcb
sampath88 <sampathreddy8896@gmail.com>
2023-07-27 13:01:24 +0530

    fix: absolute imports

--- Week-12/todo-app/src/App.js
+++ Week-12/todo-app/src/App.js
@@ -1,4 +1,4 @@
-import TodosPage from "@pages/todos-page";
+import TodosPage from "pages/todos-page";
 import "./App.css";
 
 function App() {
--- Week-12/todo-app/src/pages/todos-page.jsx
+++ Week-12/todo-app/src/pages/todos-page.jsx
@@ -1,4 +1,4 @@
-import Todo from "@components/todo";
+import Todo from "components/todo";
 
 const TodosPage = (props) => {
   return (
