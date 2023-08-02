7812ae5417058cc877bc6ade328baa5c9ecb2480
sampath88 <sampathreddy8896@gmail.com>
2023-08-02 12:38:07 +0530

    updated tailwind classes

--- Week-12/todo-app/src/components/todo.jsx
+++ Week-12/todo-app/src/components/todo.jsx
@@ -5,7 +5,7 @@ import { TodosContext } from "pages/todos-page";
 const Todo = ({ id, title, description, isDone }) => {
   const { handleComplete } = useContext(TodosContext);
   return (
-    <div className="max-w-[600px] w-full todo-item">
+    <div className="max-w-[600px] w-full group">
       <div className="m-4 mt-1">
         <div className="border border-solid border-[#e0e0e0] dark:border-[#666]  rounded-lg hover:shadow-[0_1px_2px_0_rgba(_60,_64,_67,_0.302),_0_1px_3px_1px_rgba(_60,_64,_67,_0.149)]">
           <div className="flex">
@@ -20,7 +20,7 @@ const Todo = ({ id, title, description, isDone }) => {
                 {description}
               </p>
             </div>
-            <div className={`mt-2 mr-2 checkbox ${isDone ? "" : "hidden"}`}>
+            <div className={`mt-2 mr-2 checkbox group-hover:block ${isDone ? "" : "hidden"}`}>
               <div className="flex" onClick={() => handleComplete(id)}>
                 {isDone ? (
                   <div className="w-6 h-6 flex justify-center items-center overflow-hidden cursor-pointer text-[#0652d7]">
