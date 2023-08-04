0a584b79c2ad59d3c420499717680f736de0ca9a
sampath88 <sampathreddy8896@gmail.com>
2023-08-04 20:27:03 +0530

    fixed title responsiveness

--- Week-13/url-shortener/src/pages/home.jsx
+++ Week-13/url-shortener/src/pages/home.jsx
@@ -5,14 +5,10 @@ const HomePage = () => {
   return (
     <div className="max-w-7xl mx-auto px-6">
       <div className="mt-48 mb-36">
-        <h1 className="text-6xl text-white text-center font-bold flex items-center justify-center gap-4">
-          <span>Unleash the Power of Short</span>
-          <div className="flex items-center gap-2 text-blue-600 underline underline-offset-4 cursor-pointer">
-            <span className="flex justify-center items-center w-10 h-10">
-              <LinkIcon />
-            </span>
-            <span>Links</span>
-          </div>
+        <h1 className="text-6xl text-white text-center font-bold  items-center justify-center gap-4">
+          <span className="mr-4">Unleash the Power of Short</span>
+          <LinkIcon w={"w-10"} h={"h-10"} color={'text-blue-600'} />
+          <span className="text-blue-600 underline underline-offset-4 ml-1">Links</span>
         </h1>
         <div className="flex justify-center my-3">
           <p className="text-gray-400 text-lg text-center">
