637405997210886d211a586b3a763d7d11f6b179
sampath88 <sampathreddy8896@gmail.com>
2023-08-10 17:48:35 +0530

    integrated https://shrtco.de/docs/ api

--- Week-13/url-shortener/src/App.js
+++ Week-13/url-shortener/src/App.js
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
 import HomePage from "pages/home";
 import Navbar from "components/navbar";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App bg-gray-900 antialiased">
-      <Navbar />
-      <HomePage />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App bg-gray-900 antialiased">
+        <Navbar />
+        <HomePage />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- Week-13/url-shortener/src/pages/home.jsx
+++ Week-13/url-shortener/src/pages/home.jsx
@@ -7,8 +7,10 @@ const HomePage = () => {
       <div className="mt-48 mb-36">
         <h1 className="text-6xl text-white text-center font-bold  items-center justify-center gap-4">
           <span className="mr-4">Unleash the Power of Short</span>
-          <LinkIcon w={"w-10"} h={"h-10"} color={'text-blue-600'} />
-          <span className="text-blue-600 underline underline-offset-4 ml-1">Links</span>
+          <LinkIcon w={"w-10"} h={"h-10"} color={"text-blue-600"} />
+          <span className="text-blue-600 underline underline-offset-4 ml-1">
+            Links
+          </span>
         </h1>
         <div className="flex justify-center my-3">
           <p className="text-gray-400 text-lg text-center">
