9df2c11644ebf29f8a82e963eb6fca99de428644
sampath88 <sampathreddy8896@gmail.com>
2023-09-25 11:40:30 +0530

    added typescript

--- Week-18/weather-app/src/index.ts
+++ Week-18/weather-app/src/index.ts
@@ -1,5 +1,6 @@
 require("dotenv").config();
-const app = require("./app");
+import app from  './app';
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is running at port:${PORT}`));
+
---
+++ Week-18/weather-app/src/routes/weather/controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+const API_URL_GEOCODE = process.env.API_URL_GEOCODE;
+const API_URL_WEATHER = process.env.API_URL_WEATHER;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
+const getCurrentWeather = async (req: Request, res: Response) => {
+    
+};
